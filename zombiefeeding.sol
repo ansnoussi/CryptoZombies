6d9d631e07fb1d9cce2f85608f1305e8ae2a62e1
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-03-11 18:04:47 +0100

    zombie helper contract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -29,14 +29,9 @@ contract ZombieFeeding is ZombieFactory {
     _zombie.readyTime = uint32(now + cooldownTime);
   }
 
-  function _isReady(Zombie storage _zombie) internal view returns (bool) {
-      return (_zombie.readyTime <= now);
-  }
-
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {

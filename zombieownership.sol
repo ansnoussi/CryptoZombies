ea103944636b28fc89e1492f41ce61a78ef3197a
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-03-11 22:50:03 +0100

    web interface

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,9 +1,8 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.4.25;
 
 import "./zombieattack.sol";
 import "./erc721.sol";
 import "./safemath.sol";
-
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
@@ -27,13 +26,13 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
-    _transfer(_from, _to, _tokenId);
-  }
+      require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+      _transfer(_from, _to, _tokenId);
+    }
 
   function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
-    zombieApprovals[_tokenId] = _approved;
-    emit Approval(msg.sender, _approved, _tokenId);
-  }
+      zombieApprovals[_tokenId] = _approved;
+      emit Approval(msg.sender, _approved, _tokenId);
+    }
 
 }
